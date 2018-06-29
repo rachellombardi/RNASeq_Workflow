c8139f58062baf1c6e49d29cff5a4695f30b2865
Rachel Lynn Lombardi <rachellynnlombardi@gmail.com>
2018-06-29 11:00:25 -0700

    script uses .gtf annotation file (not .gff3 file)

--- working_data/star/star_scripts/genome_indices.sh
+++ working_data/star/star_scripts/genome_indices.sh
@@ -6,8 +6,8 @@ module load star/2.5.2b
 
 ## generating genome indices
 ## completed 6/25/2018
+## script uses .gtf annotation file, not .gff3 file
 STAR --runMode genomeGenerate --genomeDir /home/lombardi/HiSeq_1143/data/working_data/star/genome \
 --genomeFastaFiles /home/lombardi/HiSeq_1143/data/working_data/star/genome/csi.chromosome.fa \
---sjdbGTFfile /home/lombardi/HiSeq_1143/data/working_data/star/annotation_GFF3/csi.gene.models.gff3 \
---sjdbGTFtagExonParentTranscript Parent --sjdbOverhang 149 --runThreadN 24 \
-
+--sjdbGTFfile /home/lombardi/HiSeq_1143/data/working_data/star/annotation_GFF3/csi.gene.models.gtf \
+--sjdbOverhang 149 --runThreadN 24 \
