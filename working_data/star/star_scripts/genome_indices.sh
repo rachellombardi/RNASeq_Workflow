a398c3e8ca1660e8124a938ef14dc14136656e62
Rachel Lynn Lombardi <rachellynnlombardi@gmail.com>
2018-06-29 11:40:51 -0700

    updated the path to the annotation file with new directory name (i.e. from annotation_GFF3/ to annotation/

--- working_data/star/star_scripts/genome_indices.sh
+++ working_data/star/star_scripts/genome_indices.sh
@@ -4,10 +4,10 @@
 module load star/2.5.2b
 
 
-## generating genome indices
-## completed 6/25/2018
+## generating genome indices for STAR 
+## completed 6/29/2018
 ## script uses .gtf annotation file, not .gff3 file
 STAR --runMode genomeGenerate --genomeDir /home/lombardi/HiSeq_1143/data/working_data/star/genome \
 --genomeFastaFiles /home/lombardi/HiSeq_1143/data/working_data/star/genome/csi.chromosome.fa \
---sjdbGTFfile /home/lombardi/HiSeq_1143/data/working_data/star/annotation_GFF3/csi.gene.models.gtf \
+--sjdbGTFfile /home/lombardi/HiSeq_1143/data/working_data/star/annotation/csi.gene.models.gtf \
 --sjdbOverhang 149 --runThreadN 24 \
