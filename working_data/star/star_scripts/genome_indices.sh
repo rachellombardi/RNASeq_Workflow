5300ffa96fec846e8c120161b73537d9719d52ba
Rachel Lynn Lombardi <rachellynnlombardi@gmail.com>
2018-06-29 10:57:31 -0700

    changed the --genomeDir path; annotation file still .gff3

--- working_data/star/star_scripts/genome_indices.sh
+++ working_data/star/star_scripts/genome_indices.sh
@@ -6,6 +6,8 @@ module load star/2.5.2b
 
 ## generating genome indices
 ## completed 6/25/2018
-STAR --runThreadN 24 --runMode genomeGenerate --genomeDir /home/lombardi/HiSeq_1143/data/working_data/star/indexed_genome --genomeFastaFiles /home/lombardi/HiSeq_1143/$
-
+STAR --runMode genomeGenerate --genomeDir /home/lombardi/HiSeq_1143/data/working_data/star/genome \
+--genomeFastaFiles /home/lombardi/HiSeq_1143/data/working_data/star/genome/csi.chromosome.fa \
+--sjdbGTFfile /home/lombardi/HiSeq_1143/data/working_data/star/annotation_GFF3/csi.gene.models.gff3 \
+--sjdbGTFtagExonParentTranscript Parent --sjdbOverhang 149 --runThreadN 24 \
 
